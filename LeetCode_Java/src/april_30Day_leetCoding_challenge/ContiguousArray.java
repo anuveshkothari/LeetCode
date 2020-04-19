d58aea3efc55095efd7fd22b0549ef0fe6a997ea
Anuvesh Kothari <anuveshkothari@gmail.com>
2020-04-19 21:17:19 +0530

    14thapril,stub

--- LeetCode_Java/src/april_30Day_leetCoding_challenge/ContiguousArray.java
+++ LeetCode_Java/src/april_30Day_leetCoding_challenge/ContiguousArray.java
@@ -2,4 +2,19 @@ package april_30Day_leetCoding_challenge;
 
 public class ContiguousArray {
 
+	public static void main(String[] args) {
+
+		ContiguousArray obj = new ContiguousArray();
+		int arr[] = { 0, 1, 0 };
+
+		System.out.println(obj.findMaxLength(arr));
+
+	}
+
+	public int findMaxLength(int[] nums) {
+
+		int result = 0;
+
+		return result;
+	}
 }
